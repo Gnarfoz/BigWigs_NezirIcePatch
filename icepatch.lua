daa464f85386f7e04c9cbdd9e45bd4da5fbcec14
Gnarfoz (Christian Wiese) <gnarfoz@gmail.com>
2014-02-06 20:38:59 +0100

    Spell ID change, options fix, TOC bump

--- icepatch.lua
+++ icepatch.lua
@@ -2,15 +2,15 @@ local mod = BigWigs:NewBoss("Conclave of Wind - Nezir Ice Patches", 773)
 if not mod then return end
 mod:RegisterEnableMob(45870, 45871, 45872) -- Anshal, Nezir, Rohash
 
-local icePatch = "~"..(GetSpellInfo(93130))
+local icePatch = "~"..(GetSpellInfo(86111))
 
 function mod:GetOptions()
-	return {93130}, {[84645] = "Nezir"}
+	return {86111}, {[86111] = -3178} -- -3178 = Nezir
 end
 
 function mod:OnBossEnable()
-	self:Log("SPELL_DAMAGE", "IcePatch", 93130)
-	self:Log("SPELL_MISSED", "IcePatch", 93130)
+	self:Log("SPELL_DAMAGE", "IcePatch", 86111)
+	self:Log("SPELL_MISSED", "IcePatch", 86111)
 	self:RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "CheckBossStatus")
 	self:Death("Disable", 45871)
 end
@@ -20,7 +20,7 @@ function mod:OnEngage(diff)
 		mod:Disable()
 		return
 	end
-	self:Bar(93130, icePatch, 15, 93130)
+	self:Bar(86111, 15, icePatch, 86111)
 end
 
 do
@@ -30,7 +30,7 @@ do
 		if (t - prev) > 10 then
 			prev = t
 			self:SendMessage("BigWigs_StopBar", icePatch)
-			self:Bar(93130, icePatch, 15, 93130)
+			self:Bar(86111, 15, icePatch, 86111)
 		end
 	end
 end
